9810849021caea92d5183e504c475a3a0d3a1f92
Vikas Sontakke <vikassontakke2002@gmail.com>
2026-04-13 00:43:58 +0530

    Unwanted Files Removed

--- src/index.js
+++ src/index.js
@@ -2,23 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-// import MainOtp from './Nodemailer/MainOtp';
-// import App from './Nodemailer/Registration';
-// import { Mail } from './Nodemailer/SampleMailComponent';
-// import MainCiit from './Forms/mainCiit';
 import MainComponent from './CIIT/MainRouting';
-// import Otp from './Nodemailer/Registerwithotp';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <MainCiit/> */}
-    {/* <MasterRouting/> */}
-    {/* <Employee/> */}
-    {/* <Mail/> */}
-    {/* <App/> */}
-    {/* <MainOtp/> */}
     <MainComponent/>
   </React.StrictMode>
 );
